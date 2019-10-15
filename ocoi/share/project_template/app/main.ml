c7a56da6bc7f7a7945eb6e9b47a97f615cf30284
Roddy MacSween <github@roddymacsween.co.uk>
2019-10-15 18:23:03 +0100

    Changes to template project

--- ocoi/share/project_template/app/main.ml
+++ ocoi/share/project_template/app/main.ml
@@ -7,4 +7,8 @@ let hello_world =
 
 let _ =
   let app = App.empty in
-  app |> hello_world |> App.run_command
+  let reporter = Logs_fmt.reporter () in
+  Logs.set_reporter reporter ;
+  Logs.set_level (Some Logs.Info) ;
+  let app = app |> hello_world in
+  app |> middleware Ocoi.Logging.default_logger |> App.run_command
