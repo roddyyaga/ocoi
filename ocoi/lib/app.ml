a21541ee359fcd2c581e03449836a1927e184d52
Roddy <github@roddymacsween.co.uk>
2020-01-31 11:59:26 +0000

    Added register list function

--- ocoi/lib/app.ml
+++ ocoi/lib/app.ml
@@ -1,4 +1,7 @@
-open Core
+open Base
 open Opium.Std
 
 let base = App.empty |> middleware Logging.default_logger
+
+let register app handlers =
+  List.fold ~init:app ~f:(fun app' handler -> handler app') handlers
--- ocoi/lib/app.mli
+++ ocoi/lib/app.mli
@@ -1,6 +1,10 @@
 (** Defines an alternative to [Opium.Std.App.empty] that already has some middleware applied *)
 
+open Base
 open Opium.Std
 
 val base : App.t
 (** [App.empty] with the default Ice logger middleware applied *)
+
+val register : Opium.App.t -> Opium.App.builder list -> Opium.App.t
+(** Apply a list of handlers to an app *)
